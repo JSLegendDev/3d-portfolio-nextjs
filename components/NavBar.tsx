24ad7e31aba134d75edfe0330c50ed8987cf33ae
Nassim <jslegend@protonmail.com>
2023-09-12 11:14:40 -0400

    fixed resume button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -52,7 +52,6 @@ export default function Navbar() {
           className={`${
             hideMobileMenu ? "hidden" : ""
           } w-full md:block md:w-auto`}
-          id="navbar-default"
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
@@ -83,7 +82,7 @@ export default function Navbar() {
             <li>
               <a
                 href="#"
-                className="block inline-flex py-2 pl-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block flex py-2 pl-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Resume
                 <span className="px-2 py-1">
