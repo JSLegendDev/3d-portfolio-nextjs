203be5d75eefcc8b4f02042abf0913010306bd59
Nassim <jslegend@protonmail.com>
2023-09-11 23:27:23 -0400

    started working on navbar + hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Hero } from "@/components/Hero";
+import Navbar from "@/components/NavBar";
+
 export default function Home() {
-  return <main className="flex flex-col"></main>;
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+        port: "",
+        pathname: "/blocks/marketing-ui/hero/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
