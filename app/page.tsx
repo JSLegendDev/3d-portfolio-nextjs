517e9d63816219e0debfa07cff467596778438f1
Nassim <jslegend@protonmail.com>
2023-09-12 11:10:37 -0400

    fixed css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col ">
       <Navbar />
       <Hero />
     </main>
